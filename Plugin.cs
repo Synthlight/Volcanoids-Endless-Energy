f4cbc500a6a6236df22b2b54a3751d4e2b1115ef
Gregory Brutsche <gregory@mail.brutsches.local>
2021-08-11 18:37:46 -0400

    Initial commit.

---
+++ Plugin.cs
@@ -0,0 +1,17 @@
+﻿using Base_Mod;
+using JetBrains.Annotations;
+
+namespace Endless_Energy {
+    [UsedImplicitly]
+    public class Plugin : BaseGameMod {
+        protected override string ModName => "Endless-Energy";
+
+        public override void OnGameLoaded() {
+            foreach (var powerPlant in GameResources.Instance.Items.WithComponent<PowerPlant>()) {
+                if (powerPlant.name == "SegmentFront") powerPlant.EnergyPerSecond = 9999;
+            }
+
+            base.OnGameLoaded();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,8 @@
+﻿# Volcanoids Endless Energy Mod
+The ship itself now produces 9999 energy which is practically endless.
+
+# Requirements
+- Volcanoids obviously.
+
+# Installation
+- See https://github.com/Synthlight/Volcanoids-Modding-Wiki/wiki#installing-mods
