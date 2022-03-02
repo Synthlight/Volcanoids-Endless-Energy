cd5842c2204b6ffefdb36f88aa117d5795c26e14
Gregory Brutsche <gregory@mail.brutsches.local>
2022-03-01 19:23:32 -0500

    Update for 1.27.

--- Plugin.cs
+++ Plugin.cs
@@ -1,21 +1,16 @@
 ﻿using Base_Mod;
 using JetBrains.Annotations;
-using UnityEngine.SceneManagement;
 
 namespace Endless_Energy {
     [UsedImplicitly]
     public class Plugin : BaseGameMod {
         protected override bool UseHarmony => true;
 
-        public override void OnGameLoaded(Scene scene) {
-            foreach (var powerPlant in RuntimeAssetDatabase.Get<ItemDefinition>().WithComponent<PowerPlant>()) {
-                if (powerPlant.name == "SegmentFront") powerPlant.EnergyPerSecond = 9999;
-            }
+        public override void OnInitData() {
             foreach (var core in RuntimeAssetDatabase.Get<TrainCoreItemDefinition>()) {
-                core.MaxEnergy = 9999;
+                core.MaxEnergy             = 9999;
+                core.BoilerEnergyPerSecond = 9999;
             }
-
-            base.OnGameLoaded(scene);
         }
     }
 }
