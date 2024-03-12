5984b3cd4af2102a6274d815086cbd640a758492
Gregory Brutsche <gregory@mail.brutsches.local>
2024-03-12 17:29:02 -0500

    Update for Volc 1.31.

--- Plugin.cs
+++ Plugin.cs
@@ -1,16 +1,16 @@
 ﻿using Base_Mod;
 using JetBrains.Annotations;
 
-namespace Endless_Energy {
-    [UsedImplicitly]
-    public class Plugin : BaseGameMod {
-        protected override bool UseHarmony => true;
+namespace Endless_Energy;
 
-        public override void OnInitData() {
-            foreach (var core in RuntimeAssetDatabase.Get<TrainCoreItemDefinition>()) {
-                core.MaxEnergy             = 9999;
-                core.BoilerEnergyPerSecond = 9999;
-            }
+[UsedImplicitly]
+public class Plugin : BaseGameMod {
+    protected override bool UseHarmony => true;
+
+    public override void OnInitData() {
+        foreach (var core in RuntimeAssetDatabase.Get<TrainCoreItemDefinition>()) {
+            core.MaxEnergy             = 9999;
+            core.BoilerEnergyPerSecond = 9999;
         }
     }
 }
